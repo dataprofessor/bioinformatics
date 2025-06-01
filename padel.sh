0ea73034046fe9ac0e7a51d664659153da030e4a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-06-01 15:44:26 -0700

    Delete padel.sh

--- padel.sh
+++
@@ -1,1 +0,0 @@
-java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml -dir ./ -file descriptors_output.csv
